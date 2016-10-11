ade0b82c57f6f524ae02d00de3fb410b8af6fcd1
stewart <stewart@snwebdev.com>
2016-10-11 14:43:42 +0100

    finalise tests and add page title

--- app/tests/components/countdown.test.jsx
+++ app/tests/components/countdown.test.jsx
@@ -9,27 +9,52 @@ var Countdown = require('Countdown');
 describe('Countdown', () => {
   it('Should exist', () => {
     expect(Countdown).toExist();
-
   });
 
   describe('handleSetCountdown', () => {
     it('should set state to started and countdown', (done) => {
       var countdown = TestUtils.renderIntoDocument(<Countdown/>);
       countdown.handleSetCountdown(10);
+
       expect(countdown.state.count).toBe(10);
       expect(countdown.state.countdownStatus).toBe('started');
+
       setTimeout(() => {
         expect(countdown.state.count).toBe(9);
         done();
       }, 1001)
     });
+
     it('should stop at 0', (done) => {
       var countdown = TestUtils.renderIntoDocument(<Countdown/>);
       countdown.handleSetCountdown(1);
+
       setTimeout(() => {
         expect(countdown.state.count).toBe(0);
         done();
       }, 3001)
+    });
+
+    it('should pause contdown on paused status', () => {
+      var countdown = TestUtils.renderIntoDocument(<Countdown/>);
+      countdown.handleSetCountdown(3);
+      countdown.handleStatusChange('paused');
+      setTimeout(() => {
+        expect(countdown.state.count).toBe(3);
+        expect(countdown.state.countdownStatus).toBe('paused');
+        done();
+      }, 1001);
+    });
+
+    it('should set count to 0 when status is stopped', () => {
+      var countdown = TestUtils.renderIntoDocument(<Countdown/>);
+      countdown.handleSetCountdown(3);
+      countdown.handleStatusChange('paused');
+      setTimeout(() => {
+        expect(countdown.state.count).toBe(0);
+        expect(countdown.state.countdownStatus).toBe('stopped');
+        done();
+      }, 1001);
     });
   });
 });
